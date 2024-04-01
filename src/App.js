e629fed0905fa9272c97ca722a08f230d78bedfb
Verity <mastersverity@gmail.com>
2024-04-01 13:01:19 +0100

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">Weather App</div>
+    </div>
+  );
 }
 
 export default App;
