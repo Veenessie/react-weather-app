912caad5d5bf9e856ab6a5a7ae12e6b4816bf7a1
Verity <mastersverity@gmail.com>
2024-04-02 14:10:27 +0100

    Created weather componant file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">Weather App</div>
+      <div className="container">
+        <Weather />
+      </div>
       <footer>
         This project was coded by{" "}
         <a href="https://github.com/Veenessie">Verity Masters</a>
