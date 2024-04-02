659df4b8e01116c0108f89448bc1fe97b64449ea
Verity <mastersverity@gmail.com>
2024-04-02 14:30:07 +0100

    Added basic html

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,34 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Weather App</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <input type="text" placeholder="Enter a city" />
+        <input type="submit" value="search" />
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Tuesday 14:13</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Weather Icon"
+          />
+          10°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 16%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
