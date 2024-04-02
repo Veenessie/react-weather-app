7ee08662bec8b7e7ca6342277cff2a14588f7951
Verity <mastersverity@gmail.com>
2024-04-02 14:07:53 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <div className="container">Weather App</div>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Veenessie">Verity Masters</a>
+        and is{" "}
+        <a href="https://github.com/Veenessie/react-weather-app">
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://verity-react-weather-app.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
