152561a8c54bbf3d1a99bd18cd58c55f984c6a6c
Verity <mastersverity@gmail.com>
2024-04-07 14:01:20 +0100

    added useeffect so forecast loads when city changes

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,18 +1,19 @@
 import React from "react";
+import "./WeatherForecastDay.css";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let max = Math.round(props.forecast[0].temperature.maximum);
+    let max = Math.round(props.forecast.temperature.maximum);
     return <span>{max}</span>;
   }
 
   function minTemperature() {
-    let min = Math.round(props.forecast[0].temperature.minimum);
+    let min = Math.round(props.forecast.temperature.minimum);
     return <span>{min}</span>;
   }
 
   function day() {
-    let time = new Date(props.forecast[0].time * 1000);
+    let time = new Date(props.forecast.time * 1000);
     let day = time.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -24,8 +25,8 @@ export default function WeatherForecastDay(props) {
       <div className="WeatherForecast-day">{day()}</div>
       <div className="WeatherForecast-icon">
         <img
-          src={props.forecast[0].condition.icon_url}
-          alt={props.forecast[0].condition.description}
+          src={props.forecast.condition.icon_url}
+          alt={props.forecast.condition.description}
         />
       </div>
       <div className="WeatherForecast-temperature">
